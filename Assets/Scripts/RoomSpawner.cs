16ad723668eeacf3012aa7655d4667d4c2e57f8c
Taryc47 <47483482+Taryc47@users.noreply.github.com>
2020-04-19 14:44:39 -0400

    Tawamon
    update

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -7,8 +7,8 @@ public class RoomSpawner : MonoBehaviour{
 	public int openingDirection;
 	// 1 = need bottom opening 
 	// 2 = need top opening 
-	// 3 = need left opening 
-	// 4 = need right opening 
+	// 3 = need right opening 
+	// 4 = need left opening 
    
 	private RoomsHalls templates;
 	private int rand;
@@ -33,13 +33,13 @@ public class RoomSpawner : MonoBehaviour{
 				rand = Random.Range(0, templates.topHalls.Length);
 				Instantiate(templates.topHalls[rand], transform.position, templates.topHalls[rand].transform.rotation);
 			}else if(openingDirection == 3){
-				// Need to spawn a hall with a LEFT opening
-				rand = Random.Range(0, templates.leftHalls.Length);
-				Instantiate(templates.leftHalls[rand], transform.position, templates.leftHalls[rand].transform.rotation);
-			}else if(openingDirection == 4){
 				// Need to spawn a hall with a RIGHT opening
 				rand = Random.Range(0, templates.rightHalls.Length);
 				Instantiate(templates.rightHalls[rand], transform.position, templates.rightHalls[rand].transform.rotation);
+			}else if(openingDirection == 4){
+				// Need to spawn a hall with a LEFT opening
+				rand = Random.Range(0, templates.leftHalls.Length);
+				Instantiate(templates.leftHalls[rand], transform.position, templates.leftHalls[rand].transform.rotation);
 				
 			}else if(openingDirection == 5){
 				// Need to spawn an extended hall with a BOTTOM opening
@@ -50,13 +50,13 @@ public class RoomSpawner : MonoBehaviour{
 				rand = Random.Range(0, templates.topExtendedHalls.Length);
 				Instantiate(templates.topExtendedHalls[rand], transform.position, templates.topExtendedHalls[rand].transform.rotation);
 			}else if(openingDirection == 7){
-				// Need to spawn an extended hall with a LEFT opening
-				rand = Random.Range(0, templates.leftExtendedHalls.Length);
-				Instantiate(templates.leftExtendedHalls[rand], transform.position, templates.leftExtendedHalls[rand].transform.rotation);
-			}else if(openingDirection == 8){
 				// Need to spawn an extended hall with a RIGHT opening
 				rand = Random.Range(0, templates.rightExtendedHalls.Length);
 				Instantiate(templates.rightExtendedHalls[rand], transform.position, templates.rightExtendedHalls[rand].transform.rotation);
+			}else if(openingDirection == 8){
+				// Need to spawn an extended hall with a LEFTopening
+				rand = Random.Range(0, templates.leftExtendedHalls.Length);
+				Instantiate(templates.leftExtendedHalls[rand], transform.position, templates.leftExtendedHalls[rand].transform.rotation);
 				
 			}else if(openingDirection == 9){
 				// Need to spawn a room with a BOTTOM opening
@@ -67,16 +67,14 @@ public class RoomSpawner : MonoBehaviour{
 				rand = Random.Range(0, templates.topRooms.Length);
 				Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
 			}else if(openingDirection == 11){
-				// Need to spawn a room with a LEFT opening
-				rand = Random.Range(0, templates.leftRooms.Length);
-				Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
-			}else if(openingDirection == 12){
 				// Need to spawn a room with a RIGHT opening
 				rand = Random.Range(0, templates.rightRooms.Length);
 				Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
-			
+			}else if(openingDirection == 12){
+				// Need to spawn a room with a LEFT opening
+				rand = Random.Range(0, templates.leftRooms.Length);
+				Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
 			}
-			
 		}	
 		spawned = true;	
 	}
